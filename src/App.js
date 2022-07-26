a3a70f58c6877248e742fe07dda426c280e1841a
NicoToff <toffolo01@hotmail.com>
2022-07-26 20:43:35 +0200

    First formula implemented & decent CSS with Bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./css/solarBootswtach.min.css";
+import { InputField, OutputField } from "./hooks/Fields.js";
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    let [nbrWords, setNbrWords] = useState(0);
+    let [nbrErrors, setNbrErrors] = useState(0);
+    nbrWords = Number(nbrWords);
+    nbrErrors = Number(nbrErrors);
+
+    let outputField;
+    if (nbrWords < nbrErrors) {
+        outputField = <OutputField value={"Erreur"} type="text" label="% erreurs" classes="is-invalid" />;
+    } else if (nbrWords <= 0) {
+        outputField = <OutputField value={"Entrez les valeurs"} type="text" label="% erreurs" />;
+    } else {
+        outputField = (
+            <OutputField
+                value={((nbrErrors / nbrWords) * 100).toFixed(2) + " %"}
+                type="text"
+                label="% erreurs"
+                classes="is-valid"
+            />
+        );
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Super React Calculator</h1>
+                <img src={logo} className="App-logo" alt="React logo" />
+                <div className="form-group">
+                    <h2>Pourcentage d'erreurs</h2>
+                    <InputField setter={setNbrWords} type="number" label="Nbr mots" small={true} />
+                    <InputField setter={setNbrErrors} type="number" label="Nbr erreurs" small={true} />
+                    {outputField}
+                </div>
+            </header>
+        </div>
+    );
 }
-
-export default App;
---
+++ src/hooks/Fields.js
@@ -0,0 +1,28 @@
+import "../css/solarBootswtach.min.css";
+
+export function InputField({ setter, type, label = "Input", small = false }) {
+    let labelClasses = "col-form-label";
+    let inputClasses = "form-control";
+    small && (labelClasses += " col-form-label-sm") && (inputClasses += " form-control-sm");
+    return (
+        <div>
+            <label className={labelClasses}>
+                {label}
+                <input onChange={event => setter(event.target.value)} type={type} className={inputClasses} />
+            </label>
+        </div>
+    );
+}
+
+export function OutputField({ value, type, label = "Output", classes }) {
+    let classesTotal = "form-control " + classes;
+
+    return (
+        <div>
+            <label className="form-label">
+                {label}
+                <input value={value} type={type} className={classesTotal} readOnly />
+            </label>
+        </div>
+    );
+}
