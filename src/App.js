b5d308496fb16342807f632596c088560cec69cd
NicoToff <toffolo01@hotmail.com>
2022-08-06 13:33:32 +0200

    Extracted the main part of the app's body to the 'CalculatorDisplay' component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
 import logo from "./img/logo.svg";
 import "./css/App.css";
 import "./css/solarBootswtach.min.css";
-import Row from "./components/Row.js";
-import Button from "./components/Button.js";
-import { generateUUID } from "./helperFunctions.js";
-import { ErrorRateFieldBox, ComplianceRateFieldBox } from "./components/composite/CompositeFieldBox.js";
-import { useState } from "react";
+import CalculatorDisplay from "./components/composite/CalculatorDisplay";
 
 export default function App() {
-    const fieldBoxCss = "border rounded p-3 m-1 col-sm";
-
-    // One can add new fields to the App with the press of a button
-    const newRow = () => (
-        <Row
-            children={[
-                <ErrorRateFieldBox classes={fieldBoxCss} key={generateUUID()} />,
-                <ComplianceRateFieldBox classes={fieldBoxCss} key={generateUUID()} />,
-            ]}
-            key={generateUUID()}
-        />
-    );
-    const [boxList, setBoxList] = useState([newRow()]);
-    const addBoxes = () => setBoxList(boxList.concat(newRow()));
-
     return (
         <div className="App">
             <nav className="App-navbar">
@@ -30,15 +11,7 @@ export default function App() {
                 <img src={logo} className="App-logo" alt="React logo" />
             </nav>
             <header className="App-body">
-                <div className="container">
-                    <div className="row">{boxList}</div>
-                </div>
-                <Button
-                    text="Rajouter des champs"
-                    onClick={addBoxes}
-                    color="success"
-                    otherClasses="btn-lg m-3"
-                />
+                <CalculatorDisplay />
             </header>
             <footer className="App-footer">Powered by React &amp; NicoToff</footer>
         </div>
---
+++ src/components/composite/CalculatorDisplay.js
@@ -0,0 +1,32 @@
+import "../../css/solarBootswtach.min.css";
+import Row from "../Row.js";
+import Button from "../Button.js";
+import { generateUUID } from "../../helperFunctions.js";
+import { ErrorRateFieldBox, ComplianceRateFieldBox } from "./CompositeFieldBox.js";
+import { useState } from "react";
+
+export default function CalculatorDisplay() {
+    const fieldBoxCss = "border rounded p-3 m-1 col-sm";
+
+    // One can add new fields to the App with the press of a button
+    const newRow = () => (
+        <Row
+            children={[
+                <ErrorRateFieldBox classes={fieldBoxCss} key={generateUUID()} />,
+                <ComplianceRateFieldBox classes={fieldBoxCss} key={generateUUID()} />,
+            ]}
+            key={generateUUID()}
+        />
+    );
+    const [boxList, setBoxList] = useState([newRow()]);
+    const addBoxes = () => setBoxList(boxList.concat(newRow()));
+
+    return (
+        <>
+            <div className="container">
+                <div className="row">{boxList}</div>
+            </div>
+            <Button text="Rajouter des champs" onClick={addBoxes} color="success" otherClasses="btn-lg m-3" />
+        </>
+    );
+}
